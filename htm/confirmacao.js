f784b032b4743c99a38498887c16254804f8d4b0
bandeirahum <71768463+bandeirahum@users.noreply.github.com>
2020-09-23 10:03:03 -0300

    Sent by DeployFC at 23/09/2020 10:03:03

---
+++ htm/confirmacao.js
@@ -0,0 +1,38 @@
+<form name=”formebit” method="get" target="_top"
+action="https://www.ebitempresa.com.br/bitrate/pesquisa1.asp">
+<input type=hidden name='empresa' value='154105'>    
+<input type="image" border="0" name="banner"
+src="https://www.ebitempresa.com.br/bitrate/banners/b154105.gif"
+alt="O que voc&ecirc; achou desta loja?" width="468" height="60">
+</form>
+<!-- Google Code for purchase Conversion Page -->
+<script language="JavaScript" type="text/javascript">
+<!--
+var google_conversion_id = 1069744304;
+var google_conversion_language = "pt_BR";
+var google_conversion_format = "1";
+var google_conversion_color = "FFFFFF";
+if (1.0) {
+  var google_conversion_value = 1.0;
+}
+var google_conversion_label = "purchase";
+//-->
+</script>
+<script language="JavaScript" src="https://www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<img height=1 width=1 border=0 src="https://www.googleadservices.com/pagead/conversion/1069744304/imp.gif?value=1.0&label=purchase&script=0">
+</noscript>
+<SCRIPT language="JavaScript" type="text/javascript">
+<!-- Yahoo! do Brasil Internet Ltda.
+window.ysm_customData = new Object();
+window.ysm_customData.conversion = "transId=,currency=,amount=";
+var ysm_accountid  = "11T34IUE7AK9MHQCLV2LRQAT4N0";
+document.write("<SCR" + "IPT language='JavaScript' type='text/javascript' " 
++ "SRC=//" + "srv1.wa.marketingsolutions.yahoo.com" + "/script/ScriptServlet" + "?aid=" + ysm_accountid 
++ "></SCR" + "IPT>");
+// -->
+</SCRIPT>
+
+                                  
+                                  
